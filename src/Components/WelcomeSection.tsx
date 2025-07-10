b99681cefc6261eeb207bbc05ef9de555d18eed5
NicLen17 <fabioramosnic@gmail.com>
2025-07-10 03:19:11 -0300

    Change bold type in Welcome Section

--- src/Components/WelcomeSection.tsx
+++ src/Components/WelcomeSection.tsx
@@ -5,7 +5,7 @@ const WelcomeSection: React.FC = () => {
   return (
     <section id="welcome" className="bg-gradient-to-r from-red-700 to-yellow-500 text-white py-32 px-4 text-center shadow-inner min-h-[60vh] flex items-center justify-center"> 
       <div className="container mx-auto">
-        <h1 className="text-5xl font-extrabold mb-6 animate-fade-in-down">
+        <h1 className="text-5xl font-bold mb-6 animate-fade-in-down">
           Enduring Education: Bridging the Pedagogical Gap
         </h1>
         <p className="text-xl mb-8 leading-relaxed animate-fade-in-up">
